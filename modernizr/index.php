5e70f698ef400b7aab66d3ad07777a7cff2bf516
Harris Wong <harris.wong@gmail.com>
2011-09-28 16:45:32 -0400

    modernizr: Added a test for box-shadowing.
    Test a div box with modernizr CSS3 box-shadow.  Doesn't seem to work on IE9 as it said it should.  Am i missing something?

---
+++ modernizr/index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <script type="text/javascript" src="http://www.modernizr.com/downloads/modernizr-latest.js"></script>
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+</head>
+
+<body>
+    <div class="box">Test</div>
+</body>
+</html>
