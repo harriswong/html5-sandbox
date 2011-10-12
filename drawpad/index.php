0789bbaccb68055d9a2a5b29818d530d2e434f2f
Harris Wong <harris.wong@gmail.com>
2011-10-12 12:11:46 -0400

    drawpad: init code.
    Contain an index page, css, and drawpad js.

---
+++ drawpad/index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <script type="text/javascript" src="../lib/jquery.js"></script>
+    <script type="text/javascript" src="drawpad.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            var container = $('#canvas');
+            init(container, 300, 300, '#ddd');
+        });
+    </script>
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+</head>
+
+<body>
+</body>
+    <div id="canvas"><p>Draw here</p></div>
+</html>
